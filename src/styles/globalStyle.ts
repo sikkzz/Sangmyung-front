27f43ce68ebb38cf4e6f94a736b29c8859c8ead0
sikkzz <sling0623@gmail.com>
2023-01-30 22:23:11 +0900

    Feat: styled-component setting

--- src/styles/globalStyle.ts
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-
-export const GlobalStyle = createGlobalStyle`
-    div{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
-`;
