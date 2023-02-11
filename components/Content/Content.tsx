72fc1a34fd01e7e8f78bef203d0eaa05eb7fa2e6
sikkzz <sling0623@gmail.com>
2023-02-11 23:49:58 +0900

    Feat: feature greeting image

--- components/Content/Content.tsx
+++ components/Content/Content.tsx
@@ -1,11 +1,29 @@
-import {Container, InnerContainer} from './ContentElements'
+import Image from "next/image";
+import {
+  Container,
+  InnerContainer,
+  GreetingContainer,
+  ImgContainer,
+  ImgWrapper,
+} from "./ContentElements";
 
-export default function Content(){
-    return (
-        <Container>
-            <InnerContainer>
-                
-            </InnerContainer>
-        </Container>
-    )
+import Img from "@/assets/test.jpg";
+
+export default function Content() {
+  return (
+    <Container>
+      <InnerContainer>
+        <GreetingContainer>
+          <ImgContainer>
+            <ImgWrapper>
+              <Image src={Img} alt="profile" width={500} height={700} style={{paddingLeft: "100px"}}/>
+            </ImgWrapper>
+            <ImgWrapper>
+              <Image src={Img} alt="profile" width={500} height={700} style={{paddingRight: "100px"}}/>
+            </ImgWrapper>
+          </ImgContainer>
+        </GreetingContainer>
+      </InnerContainer>
+    </Container>
+  );
 }
--- components/Content/ContentElements.tsx
+++ components/Content/ContentElements.tsx
@@ -1,21 +1,30 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-    padding: 96px 0;
-`
+  padding: 96px 0;
+`;
 
 const InnerContainer = styled.div`
+  max-width: 1120px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  /* height: 300px; */
+  background-color: rgb(255 255 255);
+`;
+
+const GreetingContainer = styled.div``;
+
+const ImgContainer = styled.div`
     max-width: 1120px;
-    padding-left: 20px;
-    padding-right: 20px;
-    margin-left: auto;
-    margin-right: auto;
-    height: 300px;
-    background-color: rgb(255 255 255);
+    /* background-color: #000; */
+    display: flex;
+    justify-content: space-between;
 `
 
-const GreetingContainer = styled.div`
-    
+const ImgWrapper = styled.div`
+    /* width: 350px; */
 `
 
-export {Container, InnerContainer}
+export { Container, InnerContainer, GreetingContainer, ImgContainer,ImgWrapper };
