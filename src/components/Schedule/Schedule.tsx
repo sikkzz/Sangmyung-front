fdefb8bae7abf4fd62609c8637d2fdfb3dd05547
sikkzz <sling0623@gmail.com>
2023-02-28 02:26:18 +0900

    Refactor: refactoring calendar

--- src/components/Schedule/Schedule.tsx
+++ src/components/Schedule/Schedule.tsx
@@ -1,487 +1,98 @@
-import { useState, useCallback, useEffect } from "react";
-
-import './test.css'
+import { useState } from "react";
+import moment from "moment";
 
 import {
   Layout,
   Col,
   Box,
-//   Title,
-//   Line,
-//   TextBox,
-//   Text,
-//   Frame,
-//   Header,
-//   SelectContainer,
-//   MonthContainer,
-//   Month,
-//   MonthSub,
-//   LeftArrows,
-//   RightArrows,
-//   Body,
-//   DayWeekContainer,
-//   DayContainer,
-//   DayWeek,
-//   SubContainer,
-//   TopContainer,
-//   Day,
-//   TodayCircle,
-  Option,
-  Select,
-    WeekDay,
-    Day,
-    Container,
-    SunDay,
-    SaturDay,
-    Title,
-    Pagination,
-    Button,
-    Week,
-    Dated,
+  ControlBox,
+  Table,
+  TBody,
+  Tr,
+  Td,
+  Button,
+  Span,
 } from "./ScheduleElements";
 
-// const Schedule = () => {
-//   const test = {
-//     year: new Date().getFullYear(),
-//     month: new Date().getMonth() + 1,
-//     date: new Date().getDate(),
-//     day: new Date().getDay(),
-//   };
-
-//   const week = ["일", "월", "화", "수", "목", "금", "토"];
-//   const [selectedYear, setSelectedYear] = useState(test.year);
-//   const [selectedMonth, setSelectedMonth] = useState(test.month);
-//   const dateTotalCount = new Date(selectedYear, selectedMonth, 0).getDate();
-
-//   const prevMonth = useCallback(() => {
-//     if (selectedMonth === 1) {
-//       setSelectedMonth(12);
-//       setSelectedYear(selectedYear - 1);
-//     } else {
-//       setSelectedMonth(selectedMonth - 1);
-//     }
-//   }, [selectedMonth]);
-
-//   const nextMonth = useCallback(() => {
-//     if (selectedMonth === 12) {
-//       setSelectedMonth(1);
-//       setSelectedYear(selectedYear + 1);
-//     } else {
-//       setSelectedMonth(selectedMonth + 1);
-//     }
-//   }, [selectedMonth]);
-
-//   const monthControl = useCallback(() => {
-//     //달 선택박스에서 고르기
-//     let monthArr = [];
-//     for (let i = 0; i < 12; i++) {
-//       monthArr.push(
-//         <Option key={i + 1} value={i + 1}>
-//           {i + 1}월
-//         </Option>
-//         // <option key={i + 1} value={i + 1}>
-//         //   {i + 1}월
-//         // </option>
-//       );
-//     }
-//     return (
-//       <Select onChange={changeSelectMonth} value={selectedMonth}>
-//         {monthArr}
-//       </Select>
-//       //   <select
-//       //     // className="monthSelect"
-//       //     onChange={changeSelectMonth}
-//       //     value={selectedMonth}
-//       //   >
-//       //     {monthArr}
-//       //   </select>
-//     );
-//   }, [selectedMonth]);
-
-//   const yearControl = useCallback(() => {
-//     //연도 선택박스에서 고르기
-//     let yearArr = [];
-//     const startYear = test.year - 10; //현재 년도부터 10년전 까지만
-//     const endYear = test.year + 10; //현재 년도부터 10년후 까지만
-//     for (let i = startYear; i < endYear + 1; i++) {
-//       yearArr.push(
-//         <Option key={i} value={i}>
-//           {i}년
-//         </Option>
-//       );
-//     }
-//     return (
-//       <Select onChange={changeSelectYear} value={selectedYear}>
-//         {yearArr}
-//       </Select>
-//       //   <select
-//       //     // className="yearSelect"
-//       //     onChange={changeSelectYear}
-//       //     value={selectedYear}
-//       //   >
-//       //     {yearArr}
-//       //   </select>
-//     );
-//   }, [selectedYear]);
-
-//   const changeSelectMonth = (e: React.ChangeEvent<HTMLSelectElement>) => {
-//     setSelectedMonth(Number(e.target.value));
-//   };
-//   const changeSelectYear = (e: React.ChangeEvent<HTMLSelectElement>) => {
-//     setSelectedYear(Number(e.target.value));
-//   };
-
-//   const Days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-//   const daysLeap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-//   const daysWeek = ["일", "월", "화", "수", "목", "금", "토"];
-//   const months = [
-//     "1",
-//     "2",
-//     "3",
-//     "4",
-//     "5",
-//     "6",
-//     "7",
-//     "8",
-//     "9",
-//     "10",
-//     "11",
-//     "12",
-//   ];
-
-//   const today = new Date();
-//   const [date, setDate] = useState(today);
-//   const [day, setDay] = useState(date.getDate());
-//   const [month, setMonth] = useState(date.getMonth());
-//   const [year, setYear] = useState(date.getFullYear());
-
-//   useEffect(() => {
-//     setDay(date.getDate());
-//     setMonth(date.getMonth());
-//     setYear(date.getFullYear());
-//   }, [date]);
-
-//   const getStartDayOfMonth = (date: Date) => {
-//     return new Date(date.getFullYear(), date.getMonth(), 1).getDay();
-//   };
-
-//   const [startDay, setStartDay] = useState(getStartDayOfMonth(date));
-
-//   const isLeapYear = (year: number) => {
-//     return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
-//   };
-
-//   const days = isLeapYear(date.getFullYear()) ? daysLeap : Days;
-
-//   const onClickArrow = (year: number, month: number, direct: number) => {
-//     if (month + direct === today.getMonth()) {
-//       setDate(new Date(year, month + direct, new Date().getDate()));
-//     } else {
-//       setDate(new Date(year, month + direct, 1));
-//     }
-//   };
-
-//   return (
-//     <Layout>
-//       <Col>
-//         <Box>
-//           <Title>캘린더</Title>
-//           <Line />
-//           <TextBox>
-//             <Text>학사일정을 확인하세요</Text>
-//           </TextBox>
-//           <Frame>
-//             <Header>
-//               <SelectContainer>
-//                 <h3>
-//                   {yearControl()}년 {monthControl()}월
-//                 </h3>
-//                 <button onClick={prevMonth}>◀︎</button>
-//                 <button onClick={nextMonth}>▶︎</button>
-//               </SelectContainer>
-//               <MonthContainer>
-//                 <LeftArrows
-//                   onClick={() => {
-//                     onClickArrow(year, month, -1);
-//                   }}
-//                 >
-//                   Prev
-//                 </LeftArrows>
-//                 <Month>
-//                   {months[month]}
-//                   <MonthSub>월</MonthSub>
-//                 </Month>
-//                 <RightArrows
-//                   onClick={() => {
-//                     onClickArrow(year, month, 1);
-//                   }}
-//                 >
-//                   Next
-//                 </RightArrows>
-//               </MonthContainer>
-//             </Header>
-
-//             <Body>
-//               <DayWeekContainer>
-//                 {daysWeek.map((index) => (
-//                   <DayWeek key={index}>{<strong>{index}</strong>}</DayWeek>
-//                 ))}
-//               </DayWeekContainer>
-//               <DayContainer>
-//                 {Array(days[month] + startDay)
-//                   .fill(null)
-//                   .map((item, index) => {
-//                     const d = index - (startDay - 1);
-
-//                     return (
-//                       <SubContainer key={index}>
-//                         <TopContainer>
-//                           <TodayCircle
-//                             isToday={
-//                               year === today.getFullYear() &&
-//                               month === today.getMonth() &&
-//                               d === today.getDate()
-//                             }
-//                           />
-//                           <Day
-//                             isToday={
-//                               year === today.getFullYear() &&
-//                               month === today.getMonth() &&
-//                               d === today.getDate()
-//                             }
-//                             onClick={() => setDate(new Date(year, month, d))}
-//                           >
-//                             {d > 0 ? d : ""}
-//                           </Day>
-//                         </TopContainer>
-//                       </SubContainer>
-//                     );
-//                   })}
-//               </DayContainer>
-//             </Body>
-//           </Frame>
-//         </Box>
-//       </Col>
-//     </Layout>
-//   );
-// };
-
 const Schedule = () => {
-  const today = {
-    year: new Date().getFullYear(), //오늘 연도
-    month: new Date().getMonth() + 1, //오늘 월
-    date: new Date().getDate(), //오늘 날짜
-    day: new Date().getDay(), //오늘 요일
-  };
+  const [getMoment, setMoment] = useState(moment());
 
-  const week = ["일", "월", "화", "수", "목", "금", "토"]; //일주일
-  const [selectedYear, setSelectedYear] = useState(today.year); //현재 선택된 연도
-  const [selectedMonth, setSelectedMonth] = useState(today.month); //현재 선택된 달
-  const dateTotalCount = new Date(selectedYear, selectedMonth, 0).getDate(); //선택된 연도, 달의 마지막 날짜
+  const today = getMoment;
+  const firstWeek = today.clone().startOf("month").week();
+  const lastWeek =
+    today.clone().endOf("month").week() === 1
+      ? 53
+      : today.clone().endOf("month").week();
 
-  const prevMonth = useCallback(() => {
-    //이전 달 보기 보튼
-    if (selectedMonth === 1) {
-      setSelectedMonth(12);
-      setSelectedYear(selectedYear - 1);
-    } else {
-      setSelectedMonth(selectedMonth - 1);
-    }
-  }, [selectedMonth]);
+  const calendarArr = () => {
+    let result: any[] = [];
+    // let week = firstWeek;
+    for (let week = firstWeek; week <= lastWeek; week++) {
+      result = result.concat(
+        <Tr key={week}>
+          {Array(7)
+            .fill(0)
+            .map((data, index) => {
+              let days = today
+                .clone()
+                .startOf("year")
+                .week(week)
+                .startOf("week")
+                .add(index, "day");
 
-  const nextMonth = useCallback(() => {
-    //다음 달 보기 버튼
-    if (selectedMonth === 12) {
-      setSelectedMonth(1);
-      setSelectedYear(selectedYear + 1);
-    } else {
-      setSelectedMonth(selectedMonth + 1);
-    }
-  }, [selectedMonth]);
-
-  const monthControl = useCallback(() => {
-    //달 선택박스에서 고르기
-    let monthArr = [];
-    for (let i = 0; i < 12; i++) {
-      monthArr.push(
-        <Option key={i + 1} value={i + 1}>
-          {i + 1}월
-        </Option>
-        // <option key={i + 1} value={i + 1}>
-        //   {i + 1}월
-        // </option>
+              if (moment().format("YYYYMMDD") === days.format("YYYYMMDD")) {
+                return (
+                  <Td key={index} style={{ backgroundColor: "red" }}>
+                    <Span>{days.format("D")}</Span>
+                  </Td>
+                );
+              } else if (days.format("MM") !== today.format("MM")) {
+                return (
+                  <Td key={index} style={{ backgroundColor: "gray" }}>
+                    <Span>{days.format("D")}</Span>
+                  </Td>
+                );
+              } else {
+                return (
+                  <Td key={index}>
+                    <Span>{days.format("D")}</Span>
+                  </Td>
+                );
+              }
+            })}
+        </Tr>
       );
     }
-    return (
-      <Select onChange={changeSelectMonth} value={selectedMonth}>
-        {monthArr}
-      </Select>
-      //   <select
-      //     // className="monthSelect"
-      //     onChange={changeSelectMonth}
-      //     value={selectedMonth}
-      //   >
-      //     {monthArr}
-      //   </select>
-    );
-  }, [selectedMonth]);
-
-  const yearControl = useCallback(() => {
-    //연도 선택박스에서 고르기
-    let yearArr = [];
-    const startYear = today.year - 10; //현재 년도부터 10년전 까지만
-    const endYear = today.year + 10; //현재 년도부터 10년후 까지만
-    for (let i = startYear; i < endYear + 1; i++) {
-      yearArr.push(
-        <Option key={i} value={i}>
-          {i}년
-        </Option>
-      );
-    }
-    return (
-      <Select onChange={changeSelectYear} value={selectedYear}>
-        {yearArr}
-      </Select>
-      //   <select
-      //     // className="yearSelect"
-      //     onChange={changeSelectYear}
-      //     value={selectedYear}
-      //   >
-      //     {yearArr}
-      //   </select>
-    );
-  }, [selectedYear]);
-
-  const changeSelectMonth = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedMonth(Number(e.target.value));
+    return result;
   };
-  const changeSelectYear = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedYear(Number(e.target.value));
-  };
-
-  const returnWeek = useCallback(() => {
-    //요일 반환
-    let weekArr: any[] = [];
-    week.forEach((index) => {
-      weekArr.push(
-        // <WeekDay key={index}>{index}</WeekDay>
-        <div
-          key={index}
-          className="weekday sunday saturday"
-        //   className={cx(
-        //     { weekday: true },
-        //     { sunday: v === "일" },
-        //     { saturday: v === "토" }
-        //   )}
-        >
-          {index}
-        </div>
-      );
-    });
-    return weekArr;
-  }, []);
-
-  const returnDay = useCallback(() => {
-    //알짜 반화
-    let dayArr = [];
-
-    for (const nowDay of week) {
-      const day = new Date(selectedYear, selectedMonth - 1, 1).getDay();
-      if (week[day] === nowDay) {
-        for (let i = 0; i < dateTotalCount; i++) {
-          dayArr.push(
-            // <Day key={i + 1}>{i + 1}</Day>
-            <div
-              key={i + 1}
-              className="today weekday sunday "
-            //   className={cx(
-            //     {
-            //       //오늘 날짜일 때 표시할 스타일 클라스네임
-            //       today:
-            //         today.year === selectedYear &&
-            //         today.month === selectedMonth &&
-            //         today.date === i + 1,
-            //     },
-            //     { weekday: true }, //전체 날짜 스타일
-            //     {
-            //       //전체 일요일 스타일
-            //       sunday:
-            //         new Date(
-            //           selectedYear,
-            //           selectedMonth - 1,
-            //           i + 1
-            //         ).getDay() === 0,
-            //     },
-            //     {
-            //       //전체 토요일 스타일
-            //       saturday:
-            //         new Date(
-            //           selectedYear,
-            //           selectedMonth - 1,
-            //           i + 1
-            //         ).getDay() === 6,
-            //     }
-            //   )}
-            >
-              {i + 1}
-            </div>
-          );
-        }
-      } else {
-        // dayArr.push(<WeekDay></WeekDay>);
-        dayArr.push(<div className="weekday"></div>);
-      }
-    }
-
-    return dayArr;
-  }, [selectedYear, selectedMonth, dateTotalCount]);
 
   return (
-    // <Container>
-    //   <Title>
-    //     <h3>
-    //       {yearControl()}년 {monthControl()}월
-    //     </h3>
-    //     <Pagination>
-    //       <Button onClick={prevMonth}>◀︎</Button>
-    //       <Button onClick={nextMonth}>▶︎</Button>
-    //     </Pagination>
-    //   </Title>
-    //   <Week>{returnWeek()}</Week>
-    //   <Dated>{returnDay()}</Dated>
-    // </Container>
-    <div className="container">
-    <div className="title">
-      <h3>
-        {yearControl()}년 {monthControl()}월
-      </h3>
-      <div className="pagination">
-        <button onClick={prevMonth}>◀︎</button>
-        <button onClick={nextMonth}>▶︎</button>
-      </div>
-    </div>
-    <div className="week">{returnWeek()}</div>
-    <div className="date">{returnDay()}</div>
-  </div>
-    // <Layout>
-    //   <Col>
-    //     <Box>
-    //       <div className="container">
-    //         <div className="title">
-    //           <h3>
-    //             {yearControl()}년 {monthControl()}월
-    //           </h3>
-    //           <div className="pagination">
-    //             <button onClick={prevMonth}>◀︎</button>
-    //             <button onClick={nextMonth}>▶︎</button>
-    //           </div>
-    //         </div>
-    //         <div className="week">{returnWeek()}</div>
-    //         <div className="date">{returnDay()}</div>
-    //       </div>
-    //     </Box>
-    //   </Col>
-    // </Layout>
+    <Layout>
+      <Col>
+        <Box>
+          <ControlBox>
+            <Button
+              onClick={() => {
+                setMoment(getMoment.clone().subtract(1, "month"));
+              }}
+            >
+              이전달
+            </Button>
+            <Span>{today.format("YYYY 년 MM 월")}</Span>
+            <Button
+              onClick={() => {
+                setMoment(getMoment.clone().add(1, "month"));
+              }}
+            >
+              다음달
+            </Button>
+          </ControlBox>
+          <Table>
+            <TBody>{calendarArr()}</TBody>
+          </Table>
+        </Box>
+      </Col>
+    </Layout>
   );
 };
 
--- src/components/Schedule/ScheduleElements.tsx
+++ src/components/Schedule/ScheduleElements.tsx
@@ -1,6 +1,4 @@
 import styled from "styled-components";
-import { BsFillArrowLeftCircleFill as LeftArrow } from "react-icons/bs";
-import { BsFillArrowRightCircleFill as RightArrow } from "react-icons/bs";
 
 const Layout = styled.div`
   padding-top: 30px;
@@ -15,220 +13,49 @@ const Col = styled.div`
   margin-right: auto;
 `;
 
-const Box = styled.div``;
-
-// const Title = styled.h1`
-//   color: #000;
-// `;
-
-// const Line = styled.div`
-//   border-bottom: 1px solid #000;
-// `;
-
-// const TextBox = styled.div`
-//   margin: 30px auto 0;
-// `;
-
-// const Text = styled.h3``;
-
-// const Frame = styled.div`
-//   margin: 0 auto;
-// `;
-
-// const Header = styled.div`
-//   font-size: 18px;
-//   font-weight: bold;
-//   padding: 10px 10px 5px 10px;
-//   background-color: #fff;
-// `;
-
-// const MonthContainer = styled.div`
-//   display: flex;
-//   justify-content: center;
-// `;
-
-// const SelectContainer = styled.div`
-//   display: flex;
-// `;
-
-// const Month = styled.div`
-//   margin: 0 60px;
-//   font-size: 60px;
-// `;
-
-// const MonthSub = styled.span`
-//   font-size: 30px;
-// `;
-
-// const LeftArrows = styled(LeftArrow)`
-//   display: flex;
-//   cursor: pointer;
-//   margin: 25px 0 0 0;
-//   font-size: 30px;
-// `;
-
-// const RightArrows = styled(RightArrow)`
-//   display: flex;
-//   cursor: pointer;
-//   font-size: 30px;
-//   margin: 25px 0 0 0;
-// `;
-
-// const Body = styled.div`
-//   width: 100%;
-//   display: flex;
-//   flex-direction: column;
-// `;
-
-// const DayWeekContainer = styled.div`
-//   display: flex;
-//   flex-wrap: wrap;
-// `;
-
-// const DayContainer = styled.div`
-//   display: flex;
-//   flex-wrap: wrap;
-// `;
-
-// const DayWeek = styled.div`
-//   width: 14%;
-//   height: 2rem;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-//   cursor: pointer;
-//   font-size: 16px;
-//   border: 1px solid #ccc;
-// `;
-
-// const SubContainer = styled.div`
-//   width: 14%;
-//   height: 140px;
-//   display: flex;
-//   flex-direction: column;
-//   cursor: pointer;
-//   font-size: 16px;
-//   border: 1px solid #ccc;
-// `;
-
-// const TopContainer = styled.a`
-//   display: block;
-//   position: relative;
-//   margin-top: 10px;
-//   padding-top: 50px;
-// `;
-
-// const Day = styled.span<{ isToday: boolean }>`
-//   margin: 0 40px 0 12px;
-//   position: absolute;
-//   top: 0;
-//   font-size: 1.4rem;
-//   line-height: 1;
-//   color: ${({ isToday }) => (isToday ? "#fff" : "000")};
-// `;
-
-// const TodayCircle = styled.span<{ isToday: boolean }>`
-//   position: absolute;
-//   left: 5px;
-//   bottom: 16px;
-//   width: 40px;
-//   height: 40px;
-//   border-radius: ${({ isToday }) => (isToday ? "100%" : "0")};
-//   background: ${({ isToday }) => (isToday ? "#000" : "#fff")};
-// `;
-
-const Select = styled.select``;
-
-const Option = styled.option``;
-
-const WeekDay = styled.div`
-  width: calc(500px / 7);
-  float: left;
-  text-align: center;
-  height: 50px;
-  line-height: 50px;
-`;
-
-const SunDay = styled.div`
-  color: red;
-`;
-
-const SaturDay = styled.div`
-  color: blue;
-`;
-
-const Day = styled.div``;
-
-const Container = styled.div`
-  width: 500px;
-  height: 400px;
-  margin: auto;
-  margin-top: 300px;
-  padding: 20px 20px;
-  border: 1px solid rgba(128, 128, 128, 0.267);
-  /* position: absolute;
-  left: 50%;
-  top: 50%; */
-  transform: translate(-50%, -50%);
-`;
-
-const Title = styled.div`
+const Box = styled.div`
+  font-size: 36px;
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
-const Pagination = styled.div`
-  margin-left: auto;
-  align-self: center;
-`;
-
-const Button = styled.button`
-  margin: 0 10px;
-  border: none;
-  background: none;
-`;
-
-const Week = styled.div`
+const ControlBox = styled.div`
   display: flex;
+  flex-direction: row;
 `;
 
-const Dated = styled.div`
-  margin-top: 20px;
+const Table = styled.table`
+  display: flex;
+  width: 50vw;
+  height: 50vh;
+  justify-content: center;
 `;
 
-export {
-  Layout,
-  Col,
-  Box,
-//   Title,
-//   Line,
-//   TextBox,
-//   Text,
-//   Frame,
-//   Header,
-//   SelectContainer,
-//   MonthContainer,
-//   Month,
-//   MonthSub,
-//   LeftArrows,
-//   RightArrows,
-//   Body,
-//   DayWeekContainer,
-//   DayContainer,
-//   DayWeek,
-//   SubContainer,
-//   TopContainer,
-//   Day,
-//   TodayCircle,
-  Option,
-  Select,
-    WeekDay,
-    Day,
-    Container,
-    SunDay,
-    SaturDay,
-    Title,
-    Pagination,
-    Button,
-    Week,
-    Dated,
-};
+const TBody = styled.tbody`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Tr = styled.tr`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Td = styled.td`
+  display: flex;
+  border: 1px solid gray;
+  width: 5vw;
+  height: 5vh;
+`;
+
+const Button = styled.button``;
+
+const Span = styled.span`
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+export { Layout, Col, Box, ControlBox, Table, TBody, Tr, Td, Button, Span };
