c91ff247ff4612870cbf11241498545cc2706a6a
sikkzz <sling0623@gmail.com>
2023-05-08 17:02:40 +0900

    Feat: central data page

--- src/constants/data/CentralData.tsx
+++ src/constants/data/CentralData.tsx
@@ -1,4 +1,8 @@
-import Img from "../../assets/logo1.png";
+import Img from '../../assets/central/allin.png'
+import Img2 from '../../assets/central/sawol.png'
+import Img3 from '../../assets/central/sangmyung.png'
+import Img4 from '../../assets/central/plli.jpeg'
+import Img5 from '../../assets/central/hi.png'
 
 export const CentralData1 = [
   {
@@ -14,8 +18,8 @@ export const CentralData1 = [
     title: "사범대학",
     sub: "제36대 사범대학 학생회 사월",
     sns: "Instagram: @smu_sawol",
-    img: Img,
-    alt: "img",
+    img: Img2,
+    alt: "img2",
     president: "회장: 송지민",
     vpresident: "부회장: 안민혁",
   },
@@ -23,8 +27,8 @@ export const CentralData1 = [
     title: "경영경제대학",
     sub: "2023 경영경제대학 비상대책위원회",
     sns: "Instagram",
-    img: Img,
-    alt: "img",
+    img: Img3,
+    alt: "img3",
     president: "위원장: 서윤",
     vpresident: "부위원장: 이찬우",
   },
@@ -35,8 +39,8 @@ export const CentralData2 = [
     title: "융합공과대학",
     sub: "제7대 융합공과대학 학생회 플리",
     sns: "Instagram: @smu_plli",
-    img: Img,
-    alt: "img",
+    img: Img4,
+    alt: "img4",
     president: "회장: 소나기",
     vpresident: "부회장: 박수영",
   },
@@ -44,8 +48,8 @@ export const CentralData2 = [
     title: "문화예술대학",
     sub: "제27대 문화예술대학 학생회 사월",
     sns: "Instagram: @smu_h_i",
-    img: Img,
-    alt: "img",
+    img: Img5,
+    alt: "img5",
     president: "회장: 이루리",
     vpresident: "부회장: 이윤하",
   },
