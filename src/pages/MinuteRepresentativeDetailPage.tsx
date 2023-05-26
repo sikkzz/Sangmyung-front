67643ad787df5827bb9d1daad7070a196f4156fb
Sikkzz <sling0623@gmail.com>
2023-05-26 16:31:45 +0900

    Feat: bio routing edit

--- src/pages/MinuteRepresentativeDetailPage.tsx
+++ src/pages/MinuteRepresentativeDetailPage.tsx
@@ -13,7 +13,7 @@ const MinuteRepresentativeDetailPage = () => {
     <>
       <TopHeader />
       <Header />
-      <Bio state={location.pathname.split("/")[1]} />
+      <Bio state={location.pathname.split("/")[2]} />
       <MinuteRepresentativeDetail />
       <Footer />
     </>
