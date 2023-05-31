c4f6ee5d58fa3c4b3b9309c80c93abba14f7a207
Sikkzz <sling0623@gmail.com>
2023-05-31 19:41:27 +0900

    Feat: google route change add

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
 import Routing from "./routes/Routing";
+import RouteChangeTracking from "./utils/RouteChangeTracking";
 
 function App() {
+  RouteChangeTracking();
+
   return (
     <>
       <Routing />
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import ScrollToTop from "./hook/ScrollToTop";
+// import ScrollToTop from "./hook/ScrollToTop";
+import ReactGA from 'react-ga'
+
+const TRACKING_ID = "G-4LB5WCV8HG"
+ReactGA.initialize(TRACKING_ID)
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
